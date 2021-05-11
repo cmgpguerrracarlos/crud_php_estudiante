3ee1c406b701f464e872965dd7424f37b26616dd
cmgpWLapTop <cmgpguerracarlos@gmail.com>
2021-05-11 03:54:12 -0300

    added conexion and estudiante to models

---
+++ app/controller/controller.php
@@ -0,0 +1,26 @@
+<?
+
+if($_POST['accion'] == "delete"){
+
+}
+
+if($_POST['accion'] == "update"){
+
+}
+
+if($_POST['accion'] == "add"){
+
+}
+
+if($_GET['accion'] == "delete"){
+    header("Location: /CURE/app/eliminar.php");
+}
+
+if($_GET['accion'] == "update"){
+
+}
+
+if($_GET['accion'] == "add"){
+
+}
+?>
